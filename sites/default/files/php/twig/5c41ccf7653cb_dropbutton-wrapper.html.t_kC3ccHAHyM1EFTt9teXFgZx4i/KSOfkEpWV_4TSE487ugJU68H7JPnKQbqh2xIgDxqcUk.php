05ebc5e2e1569525d25d6a8e74b50f932313dca7
Veerasekar <veerasekar.r89@gmail.com>
2019-01-18 17:37:02 +0400

    initial commit

---
+++ sites/default/files/php/twig/5c41ccf7653cb_dropbutton-wrapper.html.t_kC3ccHAHyM1EFTt9teXFgZx4i/KSOfkEpWV_4TSE487ugJU68H7JPnKQbqh2xIgDxqcUk.php
@@ -0,0 +1,88 @@
+<?php
+
+/* core/modules/system/templates/dropbutton-wrapper.html.twig */
+class __TwigTemplate_bc1912cc972f7be3f7a8288d761ffa10c96f3b92a4f4e96a50f9c2be03440210 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 14, "spaceless" => 15);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if', 'spaceless'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 14
+        if ((isset($context["children"]) ? $context["children"] : null)) {
+            // line 15
+            echo "  ";
+            ob_start();
+            // line 16
+            echo "    <div class=\"dropbutton-wrapper\">
+      <div class=\"dropbutton-widget\">
+        ";
+            // line 18
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+            echo "
+      </div>
+    </div>
+  ";
+            echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/dropbutton-wrapper.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  52 => 18,  48 => 16,  45 => 15,  43 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/modules/system/templates/dropbutton-wrapper.html.twig", "E:\\xampp\\htdocs\\sph\\core\\modules\\system\\templates\\dropbutton-wrapper.html.twig");
+    }
+}
---
+++ sites/default/files/php/twig/5c41ccf7653cb_views-exposed-form.html.t_zq3XBQKXkGFZl-7yslhysQoLO/3b-w1fCIEPjonn3oa-pHgCkuEcSjmu1Fpa8KB1Uu3ck.php
@@ -0,0 +1,84 @@
+<?php
+
+/* core/modules/views/templates/views-exposed-form.html.twig */
+class __TwigTemplate_ea905fbad29df1363c92550800d7369bbce62767abac8f76e7f73e64383f9ef3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 14);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 14
+        if ( !twig_test_empty((isset($context["q"]) ? $context["q"] : null))) {
+            // line 15
+            echo "  ";
+            // line 19
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["q"]) ? $context["q"] : null), "html", null, true));
+            echo "
+";
+        }
+        // line 21
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["form"]) ? $context["form"] : null), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/views/templates/views-exposed-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  52 => 21,  47 => 19,  45 => 15,  43 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/modules/views/templates/views-exposed-form.html.twig", "E:\\xampp\\htdocs\\sph\\core\\modules\\views\\templates\\views-exposed-form.html.twig");
+    }
+}
